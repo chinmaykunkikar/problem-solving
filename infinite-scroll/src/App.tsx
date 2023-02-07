6cbf9492be4c245f98d37a6153ba3de0e58e5293
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-02-07 20:51:31 +0530

    infinite-scroll: Implement infinite scroll - Use IntersectionObserver to detect if the last element of the array is visible. if yes, then increment the page number

--- infinite-scroll/src/App.tsx
+++ infinite-scroll/src/App.tsx
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import useBookSearch from "./hooks/useBookSearch";
 
 export default function App() {
   const [query, setQuery] = useState<string>("");
   const [pageNumber, setPageNumber] = useState<number>(1);
   const { loading, error, books, hasMore } = useBookSearch(query, pageNumber);
+
+  const observer = useRef<IntersectionObserver>();
+  const lastBookElRef = useCallback(
+    (node: any) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevPgNo) => prevPgNo + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
 
   function handleSearch(e: React.ChangeEvent<HTMLInputElement>) {
     setQuery(e.target.value);
@@ -19,9 +34,17 @@ export default function App() {
         id="search"
         autoComplete="off"
         placeholder="Start typing a query"
+        value={query}
         onChange={handleSearch}
       />
-      {books?.map((book) => {
+      {books?.map((book, index) => {
+        if (books.length === index + 1) {
+          return (
+            <div ref={lastBookElRef} key={book}>
+              {book}
+            </div>
+          );
+        }
         return <div key={book}>{book}</div>;
       })}
       <div>{loading ? "Loading..." : ""}</div>
