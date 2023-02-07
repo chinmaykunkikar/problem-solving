56a54d1f7a91c59d6b809e863ba3b98c2f395b2c
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-02-07 16:21:48 +0530

    smallprojects: Setup infinite scroll boilerplate

---
+++ infinite-scroll/src/App.tsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <>
+      <input type="text" name="search" id="search" autoComplete="off" />
+      <div>sometext</div>
+      <div>sometext</div>
+      <div>sometext</div>
+      <div>sometext</div>
+      <div>sometext</div>
+      <div>Loading...</div>
+      <div>Error</div>
+    </>
+  );
+}
